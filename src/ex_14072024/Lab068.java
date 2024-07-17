6967adfa37f01bedcf01907302a69e440dbbe619
HARSH RAJ <harshraj278@gmail.com>
2024-07-17 15:10:59 +0530

    Added Readme File

---
+++ src/ex_14072024/Lab068.java
@@ -0,0 +1,20 @@
+package ex_14072024;
+
+public class Lab068 {
+    public static void main(String[] args) {
+        String name = "Pramod"; // Assignment Operators
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(5));
+        System.out.println(name.length());
+        //String name2 = new String("Pramod"); // New Operator
+
+        // Both of them, actually save the name, name2 in different ways JVM
+        // How they store the values in the JVM?
+
+
+        // SCP nad Heap - JVM
+
+    }
+}
