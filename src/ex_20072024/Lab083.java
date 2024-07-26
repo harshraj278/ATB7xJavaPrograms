50d9e8af083955c57b1b992c200e338a6163e3b6
HARSH RAJ <harshraj278@gmail.com>
2024-07-26 21:33:54 +0530

    Till Functions

---
+++ src/ex_20072024/Lab083.java
@@ -0,0 +1,15 @@
+package ex_20072024;
+
+public class Lab083 {
+    public static void main(String[] args) {
+        int a  = 10;
+        // a = 4 -> TRUE OR FASLE
+        if(a==10) {
+            System.out.println("HaHaHa");
+        }
+            else{
+                System.out.println("LOL");
+            }
+        }
+    }
+
