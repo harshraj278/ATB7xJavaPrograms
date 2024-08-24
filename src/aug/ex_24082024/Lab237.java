6668e5b9742964de7665373b5b86d1c1cff3000c
HARSH RAJ <harshraj278@gmail.com>
2024-08-24 14:44:07 +0530

    Collection Framework Part 2- 24th Aug 2024

---
+++ src/aug/ex_24082024/Lab237.java
@@ -0,0 +1,24 @@
+package aug.ex_24082024;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+
+public class Lab237 {
+    public static void main(String[] args) {
+        // Vector, Stack - Legacy - 95% of time we are not going to use it in automation
+        // legacy? - old ->
+        Stack s = new Stack();
+        s.add("Pramod");
+        s.add("Dutta");
+        s.add("Lucky");
+        s.add("Amit");
+        s.add("Amit2");
+        System.out.println(s);
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s);
+
+    }
+}
